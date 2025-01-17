4003cf91b1e2faa777523b5cb47269498fef98f8
AliciaMaiz <aliciamaizcurrena@gmail.com>
2025-01-17 11:58:23 +0100

    Alquila bd

--- CatalogoDePeliculas/src/db/SQLite.java
+++ CatalogoDePeliculas/src/db/SQLite.java
@@ -7,7 +7,6 @@ import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.Collection;
